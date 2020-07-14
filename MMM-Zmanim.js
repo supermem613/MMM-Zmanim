7eec7d45d15fcbccd754835d217d17cdf651567d
Jesse Seidman <seidmanjesse@gmail.com>
2020-07-13 23:25:46 -0400

    alot of changes

---
+++ MMM-Zmanim.js
@@ -0,0 +1,125 @@
+Module.register("MMM-Zmanim", {
+    defaults: {
+        timeZoneId: "America/New_York",
+        locationName: "Teaneck",
+        latitude: 40.884819,
+        longitude: -74.006561,
+        elevation: 0,
+        displayedFields: {
+            'AlosHashachar': 'Alos Hashachar',
+            'Sunrise': 'Netz Hachama',
+            'SofZmanShmaMGA': 'Zman Shma (MGA)',
+            'SofZmanShmaGRA': 'Zman Shma (GRA)',
+            'SofZmanTfilaMGA': 'Zman Tefila (MGA)',
+            'SofZmanTfilaGRA': 'Zman Tefila (GRA)',
+            'Chatzos': 'Chatzos',
+            'CandleLighting': 'Candle Lighting',
+            'Sunset': 'Shkiya',
+            'Tzais': 'Tzais',
+        },
+        displaysPastZmanim: true,
+        graysOutPastZmanim: false,
+        inIsrael: false,
+        showsHebrewDate: true,
+        showsParsha: true,
+        showsDaf: true,
+        showsDayOfOmer: true,
+        showsSpecialShabbos: true,
+        showsYomTov: true,
+        alwaysShowDividerLine: false
+    },
+
+    // Define required scripts.
+	getStyles: function () {
+		return ["Zmanim.css"];
+	},
+    
+    getDom: function() {
+        var self = this;
+        var table = document.createElement("table");
+        table.className = "small";
+
+        for (value in this.calendarArray) {
+            var row = document.createElement("tr");
+            table.appendChild(row);
+
+            var cell = document.createElement("th");
+			cell.className = "calendarValue";
+            cell.innerHTML = this.calendarArray[value];
+            cell.colSpan = 2;
+            row.appendChild(cell);
+
+            if (value == this.calendarArray.length - 1) {
+                cell.style.borderBottom = "1px solid white";
+                cell.style.paddingBottom = "10px";
+            }
+        }
+
+        for (index in this.zmanimArray) {
+            const zmanEntry = this.zmanimArray[index];
+            const currentDate = new Date();
+            const zmanHasPassed = new Date(zmanEntry[1]) < currentDate.getTime();
+            const arrayCount = this.zmanimArray.length;
+            const baseOpacity = 0.2;
+            const capOpacity = 0.4;
+            const isFirstRow = index == 0;
+            
+            var row = document.createElement("tr");
+            if (self.config.displaysPastZmanim && self.config.graysOutPastZmanim) {
+                row.style.opacity = zmanHasPassed ? (baseOpacity + (capOpacity - baseOpacity) * index / arrayCount) : 1.0;
+            } else if (!self.config.displaysPastZmanim && zmanHasPassed) {
+                continue;
+            }
+            
+            table.appendChild(row);
+
+            var titleCell = document.createElement("td");
+			titleCell.className = "zmanimTitle";
+			titleCell.innerHTML = zmanEntry[0] + ": ";
+			row.appendChild(titleCell);
+
+			var valueCell = document.createElement("td");
+			valueCell.className = "zmanimValue";
+			valueCell.innerHTML = zmanEntry[2];
+            row.appendChild(valueCell);
+            
+            if (isFirstRow) {
+                titleCell.style.paddingTop = "10px";
+                valueCell.style.paddingTop = "10px";
+
+                if (self.config.alwaysShowDividerLine && this.calendarArray.length == 0) {
+                    titleCell.style.borderTop = "1px solid white";
+                    valueCell.style.borderTop = "1px solid white";
+                }
+            }
+        }
+        
+        return table
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        if (payload.id == this.identifier) {
+            switch(notification) {
+                case "FETCHED_ZMANIM":
+                  this.zmanimArray = payload.zmanim;
+                  this.calendarArray = payload.calendar;
+                  this.updateDom();
+                  break
+              }
+        }
+    },
+
+    start: function () {
+        var self = this
+
+        self.zmanimArray = [{ subject: 'LOADING_ENTRIES' }]
+
+        // update tasks every 60s
+        var refreshFunction = function () {
+            self.config['id'] = self.identifier;
+            self.sendSocketNotification('FETCH_ZMANIM', self.config)
+        }
+        refreshFunction()
+        setInterval(refreshFunction, 60000)
+    }
+});
---
+++ README.md
@@ -0,0 +1,71 @@
+# MMM-Zmanim (MagicMirrorModule)
+Display Daily Jewish times and events from the [KosherZmanim API](https://github.com/KosherJava/zmanim) in your [MagicMirror](https://github.com/MichMich/MagicMirror).
+
+![Screenshot](screenshot.png)
+
+## Installation
+1. Go to `MagicMirror/modules`
+2. Execute `git clone https://github.com/jseidman95/MMM-Zmanim.git`
+
+## Using the module
+To use this module, add it to the modules array in the `config/config.js` file:
+```
+modules: [
+    {
+        module: "MMM-Zmanim",
+        position: "top_left", // This can be any region
+        config: {
+            // See 'Configuration options' for more information.
+        }
+    }
+]
+```
+
+## Configuration options
+The following properties can be configured:
+
+| Option                  | Default                       | Description
+| -----------------       | ------------------            | ----------- 
+| `timeZoneId`            | `America/New_York`            | Time Zone Id based on https://docs.oracle.com/middleware/12212/wcs/tag-ref/MISC/TimeZones.html
+| `locationName`          | `Teaneck`                     | The name of the city/state/town etc. to show the zmanim for
+| `latitude`              | `40.884819`                   | Latitude of the location to show the zmanim for
+| `longitude`             | `-74.006561`                  | Longitude of the location to show the zmanim for
+| `elevation`             | `0`                           | Elevation of the location to show the zmanim for
+| `displayedFields`       | `see displayedFields section` | The displayed zmanim fields
+| `displaysPastZmanim`    | `true`                        | Boolean that controls whether zmanim times that have passed are displayed
+| `graysOutPastZmanim`    | `false`                       | Boolean that controls whether zmanim that have passed are displayed with a grayed out UI
+| `inIsrael`              | `false`                       | Boolean that adjusts the zmanim for Israel or not
+| `showsHebrewDate`       | `true`                        | Boolean that controls if the hebrew date is shown at the top
+| `showsParsha`           | `true`                        | Boolean that controls if this upcoming weeks parsha is shown at the top
+| `showsDaf`              | `true`                        | Boolean that controls if today's daf is shown at the top
+| `showsDayOfOmer`        | `true`                        | Boolean that controls if today's omer count (if applicable) is shown at the top
+| `showsSpecialShabbos`   | `true`                        | Boolean that controls if the upcoming special shabbos (if applicable) is shown at the top
+| `showsYomTov`           | `true`                        | Boolean that controls if today's yom tov (if applicable) is shown at the top
+| `alwaysShowDividerLine` | `false`                       | Boolean that controls if the divider line is always shown (even if no top content is shown)
+
+## displayedFields summary
+displayedFields is a property in the config that controls the zmanim displayed and their names.  Its type is a JSON object with the 
+following structure:
+```
+    {
+        <ZmanAPIName>: <DisplayedZmanName>,
+        <ZmanAPIName>: <DisplayedZmanName>
+        ...
+    }
+```
+Where `<ZmanAPIName>` is the `KosherZmanim` field name where the possible options are [KosherZmanim API](https://github.com/KosherJava/zmanim);
+and `<DisplayedZmanName>` is the name that will be displayed and can be whatever you choose
+
+## Credits
+- MagicMirror: https://github.com/MichMich/MagicMirr
+
+: ,
+: ,
+: ,
+: ,
+: ,
+: ,
+: ,
+: ,
+: ,
+: 
